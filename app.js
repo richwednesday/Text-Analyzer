3ef1a5872c569a33f548cc27f2710b40f29076fa
richwednesday <iwokjoseph@gmail.com>
2017-02-21 09:17:15 +0100

    changes to commit

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
-function wordCount(){
-	var typedWord = $(".js-text-content").val().split(" ").length;
-	return typedWord;
+function wordCount(text){
+	return text.length;
 }
 
-function uniqueWordCount(){
+function uniqueWordCount(text){
 	var valuesSoFar = [];
-	var theWord = $(".js-text-content").val().split(" ");
-	for(var i = 0; i < theWord.length; i++){
-		if(valuesSoFar.indexOf(theWord[i]) == -1) {
-			valuesSoFar.push(theWord[i]);
+	for(var i = 0; i < text.length; i++){
+		if(valuesSoFar.indexOf(text[i]) == -1) {
+			valuesSoFar.push(text[i]);
 		}
 	}
+	
 	return valuesSoFar.length;
 }
 
-function averageWordLength(){
-	var theWord = $(".js-text-content").val().split(" ");
-	var sumOfWords = 0;
-	for(var i = 0; i < theWord.length; i++){
-		sumOfWords += theWord[i].length;
-	}
-	return sumOfWords/theWord.length; 
+function averageWordLength(text){
+	return text.reduce(function(acc, currentVal){
+		return acc + currentVal.length;
+	}, 0) / text.length;
 }
 
 $(document).ready(function(){
-	$(".js-submit").click(function(){
-		$(".js-word-count").text(wordCount());
-		$(".js-unique-word").text(uniqueWordCount());
-		$(".js-average-word").text(averageWordLength());
-		$("dl").removeClass("hidden");		
-		return false;
+	$('.js-form').on('click', '.js-submit', function(){
+		var theArray = $(this).parents('.js-form').find('.js-text-content').val().split(' ');
+
+		$(this).parents('.js-form').next().children(".js-word-count").text(wordCount(theArray));
+		$(this).parents('.js-form').next().children(".js-unique-word").text(uniqueWordCount(theArray));
+		$(this).parents('.js-form').next().children(".js-average-word").text(averageWordLength(theArray));
+		$(this).parents('.js-form').next().removeClass("hidden");	
+		return false;	
 	});
 });
